48c3a16b0e78177e8c97e041167458942dbbf153
Boštjan Kamnik <bostjan.kamnik@troia.si>
2021-11-08 13:48:20 +0100

    add time lib and parse time

--- bomb.ino
+++ bomb.ino
@@ -1,3 +1,4 @@
+#include <TimeLib.h>
 
 class Button
 {
@@ -47,6 +48,10 @@ enum BOMB_STATE
   DEFUSED
 };
 
+const int DP_LATCH_PIN = 5;        // Latch pin of 74HC595 is connected to Digital pin 5
+const int DP_CLK_PIN = 6;          // Clock pin of 74HC595 is connected to Digital pin 6
+const int DP_DIGIT_0_DATA_PIN = 4; // Data pin of 74HC595 is connected to Digital pin 4
+
 const int BOMB_WIRE_ONE_PIN = 12;
 const int BOMB_WIRE_TWO_PIN = 11;
 const int SET_BOMB_TIME_BTN_PIN = 10;
@@ -59,10 +64,10 @@ const int GREEN_LED_PIN = 3;
 BOMB_STATE cur_state = BOMB_STATE::IDLE;
 Button arm_bomb_btn(ARM_BOMB_BTN_PIN), set_bomb_time_btn(SET_BOMB_TIME_BTN_PIN);
 
-unsigned long bomb_explode_time = 0;
+unsigned long bomb_explode_duration = 0;
 unsigned long MAX_BOMB_TIME = 6UL * 3600UL * 1000UL; // 6 hrs
 
-unsigned long bomb_timer = 0;
+time_t bomb_started_time = 0;
 
 void switch_state(BOMB_STATE state)
 {
@@ -83,6 +88,11 @@ void setup()
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
 
+  // Set all the pins of 74HC595 as OUTPUT
+  pinMode(DP_LATCH_PIN, OUTPUT);
+  pinMode(DP_CLK_PIN, OUTPUT);
+  pinMode(DP_DIGIT_0_DATA_PIN, OUTPUT);
+
   Serial.begin(9600);
   Serial.println("Begin");
 }
@@ -99,20 +109,20 @@ void loop()
 
     if (set_bomb_time_btn.transitioned_to(LOW))
     {
-      const unsigned long set_timer_step_millis = 30UL * 60UL * 1000UL; // 30 min
-      bomb_explode_time += set_timer_step_millis;
+      const unsigned long set_timer_step_sec = 30UL * 60UL; // 30 min
+      bomb_explode_duration += set_timer_step_sec;
       tone(BUZZER_PIN, 700, 100);
-      if (bomb_explode_time > MAX_BOMB_TIME)
+      if (bomb_explode_duration > MAX_BOMB_TIME)
       {
-        bomb_explode_time = 0;
+        bomb_explode_duration = 0;
       }
 
-      Serial.println(String("Set timer to: ") + String(bomb_explode_time / 60 / 1000UL) + String(" minutes"));
+      Serial.println(String("Set timer to: ") + String(bomb_explode_duration / 60 ) + String(" minutes"));
     }
     else if (arm_bomb_btn.transitioned_to(LOW))
     {
       switch_state(BOMB_STATE::OPERATIONAL);
-      bomb_timer = 0;
+      bomb_started_time = now();
 
       digitalWrite(RED_LED_PIN, LOW);
       tone(BUZZER_PIN, 1300, 350);
@@ -125,8 +135,6 @@ void loop()
   }
   else if (cur_state == BOMB_STATE::OPERATIONAL)
   {
-    bomb_timer += millis();
-
     if (digitalRead(BOMB_WIRE_ONE_PIN) == HIGH)
     {
       switch_state(BOMB_STATE::DEFUSED);
@@ -147,7 +155,7 @@ void loop()
       digitalWrite(GREEN_LED_PIN, HIGH);
       tone(BUZZER_PIN, 1300, 350);
     }
-    else if (bomb_timer > bomb_explode_time)
+    else if (now() > bomb_started_time + bomb_explode_duration)
     {
       switch_state(BOMB_STATE::EXPLODED);
       // TODO: play explode sfx
@@ -156,6 +164,7 @@ void loop()
     {
       // Pulse
       tone(BUZZER_PIN, 1300, 100);
+      update_display();
     }
   }
   else if (cur_state == BOMB_STATE::DEFUSED)
@@ -174,4 +183,40 @@ void loop()
   {
     delay(100);
   }
+}
+
+// A B C D E F G DP	Numbers	HEX code
+// ---------------------------------
+// 1 1 1 1 1 1 0 0	    0	      0x3F
+// 0 1 1 0 0 0 0 0	    1	      0x06
+// 1 1 0 1 1 0 1 0	    2	      0x5B
+// 1 1 1 1 0 0 1 0	    3	      0x4F
+// 0 1 1 0 0 1 1 0	    4	      0x66
+// 1 0 1 1 0 1 1 0	    5	      0x6D
+// 1 0 1 1 1 1 1 0	    6	      0x7D
+// 1 1 1 0 0 0 0 0	    7	      0x07
+// 1 1 1 1 1 1 1 0	    8	      0x7F
+// 1 1 1 1 0 1 1 0	    9	      0x67
+void update_display()
+{
+  auto time_left = bomb_started_time + bomb_explode_duration - now();
+  if (time_left < 0)
+  {
+    time_left = 0;
+  }
+
+  int h = hour(time_left);
+  int min = minute(time_left);
+  int sec = second(time_left);
+
+  Serial.print(h);
+  Serial.print(":");
+  Serial.print(min);
+  Serial.print(":");
+  Serial.print(sec);
+  Serial.println();
+
+  digitalWrite(DP_LATCH_PIN, LOW);
+  // shiftOut(DP_DIGIT_0_DATA_PIN, DP_CLK_PIN, LSBFIRST, leds);
+  digitalWrite(DP_LATCH_PIN, HIGH);
 }
